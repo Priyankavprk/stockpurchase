6799c8e05130ff295a3585d44ef0d93ad358071a
priyanka <priyankap@rigbot.com>
2019-07-06 19:44:40 +0530

    Create the graph component

---
+++ app/components/graph.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { LineChart, Grid } from 'react-native-svg-charts';
+import { View, StyleSheet  } from 'react-native';
+
+class Graph extends Component {
+
+    render() {
+
+        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+        return (
+
+            <LineChart
+                style={styles.GraphStyle}
+                data={ data }
+                showGrid={ false }
+                svg={{ stroke: 'rgb(134, 65, 244)' }}
+                contentInset={{ top: 20, bottom: 20 }}
+            >
+                <Grid/>
+            </LineChart>
+        )
+    }
+
+}
+
+const styles = StyleSheet.create({
+  GraphStyle: {
+    height: 200,
+    margin: 20
+  }
+});
+
+export default Graph;
