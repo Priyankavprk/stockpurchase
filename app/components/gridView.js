ae0211b04296bbcea1f5052fbdb84e3a3abb0cc5
priyanka <priyankap@rigbot.com>
2019-07-06 16:40:48 +0530

    Remove state value and take initial data from the store

--- app/components/gridView.js
+++ app/components/gridView.js
@@ -1,52 +1,20 @@
 
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, FlatList, Text, View, Alert, Platform } from 'react-native';
+import { connect } from 'react-redux';
 
 import GridItem from './gridItem';
 
 class gridView extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-    items: [
-     {key: 'One'},
-     {key: 'Two'},
-     {key: 'Three'},
-     {key: 'Four'},
-     {key: 'Five'},
-     {key: 'Six'},
-     {key: 'Seven'},
-     {key: 'Eight'},
-     {key: 'Nine'},
-     {key: 'Ten'},
-     {key: 'Eleven'},
-     {key: 'Twelve'},
-     {key: 'Thirteen'},
-     {key: 'Fourteen'},
-     {key: 'Fifteen'},
-     {key: 'Sixteen'},
-     {key: 'Seventeen'},
-     {key: 'Eighteen'},
-     {key: 'Nineteen'},
-     {key: 'Twenty'},
-     {key: 'Twenty one'},
-     {key: 'Twenty two'},
-     {key: 'Twenty three'},
-     {key: 'Twenty four'},
-     {key: 'Twenty five'},
-     {key: 'Twenty six'},
-     {key: 'Twenty seven'},
-     {key: 'Twenty eight'},
-     {key: 'Twenty nine'},
-     {key: 'Thirty'}
-   ]}
   }
 
   render() {
     return (
       <View style={styles.MainContainer}>
         <FlatList
-          data={this.state.items}
+          data={this.props.items}
           renderItem={({item}) => <GridItem item={item}/>}
           numColumns={3}
         />
@@ -62,4 +30,8 @@ const styles = StyleSheet.create({
   }
 });
 
-export default gridView;
+const mapStateToProps = state => ({
+  items: state.items
+});
+
+export default connect(mapStateToProps)(gridView);
