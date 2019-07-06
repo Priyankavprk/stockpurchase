33b7b9389be264d453fde02edd76397ac9b7222f
priyanka <priyankap@rigbot.com>
2019-07-06 20:32:33 +0530

    Update graph to take data from the store and hence plot the line

--- app/components/graph.js
+++ app/components/graph.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import { LineChart, Grid } from 'react-native-svg-charts';
 import { View, StyleSheet  } from 'react-native';
+import { connect } from 'react-redux';
 
 class Graph extends Component {
 
     render() {
-
-        const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+        const data = this.props.items.filter((data) => data.stockPrice).map((d) => d.stockPrice)
 
         return (
 
@@ -31,4 +31,8 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Graph;
+const mapStateToProps = state => ({
+  items: state.items
+});
+
+export default connect(mapStateToProps)(Graph);
