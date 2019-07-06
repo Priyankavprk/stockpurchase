04255130e2e986dc1abf5567f5b5737ae15f5085
priyanka <priyankap@rigbot.com>
2019-07-06 09:03:30 +0530

    Add homescreen view

--- app/views/homeScreen.js
+++ app/views/homeScreen.js
@@ -1,1 +1,58 @@
 
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+class HomeScreen extends Component {
+ render() {
+   return (
+      <View>
+        <Button title='Click me' onPress={() => this.props.navigation.navigate('ScreenTwo')}/>
+      </View>
+   );
+ }
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default HomeScreen;
