6a435083a099447006493942be04498618dc512f
priyanka <priyankap@rigbot.com>
2019-07-06 09:28:32 +0530

    Remove unwanted code

--- app/views/homeScreen.js
+++ app/views/homeScreen.js
@@ -1,22 +1,6 @@
 
 import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { StyleSheet, ScrollView, View, Text, Button } from 'react-native';
 
 class HomeScreen extends Component {
  render() {
@@ -29,30 +13,7 @@ class HomeScreen extends Component {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  
 });
 
 export default HomeScreen;
