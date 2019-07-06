52adc2780e6d901c60236f0f429c68ebb9ff4181
priyanka <priyankap@rigbot.com>
2019-07-06 09:03:02 +0530

    Configure navigation and set up two initails screens

--- app/setup.js
+++ app/setup.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { createAppContainer } from 'react-navigation';
 
 import App from './App';
 import configureStore from './configureStore';
 import store from './configureStore';
 
+const AppContainer = createAppContainer(App);
+
 export default class Root extends Component {
   render() {
-    return <Provider store={store}><App /></Provider>
+    return <Provider store={store}><AppContainer /></Provider>
   }
 }
