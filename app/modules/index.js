b302156e1ab9671a94080d701d9ac90184181c44
priyanka <priyankap@rigbot.com>
2019-07-07 15:39:20 +0530

    Add api fetch to get all the data from airtable

--- app/modules/index.js
+++ app/modules/index.js
@@ -4,16 +4,16 @@ var Airtable = require('airtable');
 var base = new Airtable({apiKey: config.AIRTABLE_API_KEY}).base('appmKbfPqv55DJonJ');
 
 export function getData() {
+  let data = [];
   base('stockbydate').select({
-      maxRecords: 30,
-      view: "Grid view"
-  }).eachPage(function page(records, fetchNextPage) {
-
-      records.forEach(function(record) {
-          console.log('Retrieved', record);
-      });
-      fetchNextPage();
-  }, function done(err) {
-      if (err) { console.error(err); return; }
-  });
+    view: 'Grid view'
+  }).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+    records.forEach(function(record) {
+        data.push({
+          date: record.get('date'),
+          price: record.get('price')
+        })
+    });
+  })
 }
